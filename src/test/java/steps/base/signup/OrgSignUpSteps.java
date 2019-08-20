a4fd86cc95e9d8926a97731005cbfd412811658d
saoramx <saora.mx@gmail.com>
2019-08-20 02:24:50 -0500

    Eliminacion de clases no utilizadas

--- src/test/java/steps/base/signup/OrgSignUpSteps.java
+++ src/test/java/steps/base/signup/OrgSignUpSteps.java
@@ -1,12 +1,10 @@
 package steps.base.signup;
 
 import net.thucydides.core.annotations.Step;
-import steps.com.GetDataFromSpreadSheet;
 import steps.com.SignUpData;
 
 public class OrgSignUpSteps {
     private String actor;
-    private GetDataFromSpreadSheet fillDataFromSpreadSheet;
     private SignUpData signUpData;
 
     @Step("#actor should be able to send the Org Information")
